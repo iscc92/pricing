ee848797eda43fe5f4a8c84bfc8d09e041627edb
Inês Calçoa <52913315+iscc92@users.noreply.github.com>
2022-04-19 18:17:35 +0100

    Add files via upload

---
+++ src/test/java/com/calcoa/pricing/PricingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calcoa.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
