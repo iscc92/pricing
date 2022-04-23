741f4a5052d9637ae18997b819da155b22eff417
Inês <ines.calcoa@gmail.com>
2022-04-23 11:08:17 +0100

    data layer: docker-compose, SQL scripts to initiate and populate tables, application.properties binding and pom.xml dependencies

---
+++ sql/create_tables.sql
@@ -0,0 +1,42 @@
+-- Create SERVICES table
+CREATE TABLE if not exists Services (
+    name varchar(25) NOT NULL,
+    price decimal NOT NULL,
+    type varchar(25) NOT NULL,
+    PRIMARY KEY (name)
+);
+
+-- Create CUSTOMERS table
+CREATE TABLE if not exists Customers (
+    customerID varchar(25) NOT NULL,
+    name varchar(25) NOT NULL,
+    number_free_days int,
+    price_service_A float,
+    price_service_B float,
+    price_service_C float,
+    discount_service_A float,
+    discount_service_B float,
+    discount_service_C float,
+    start_service_A date,
+    start_service_B date,
+    start_service_C date,
+    PRIMARY KEY (customerID)
+);
+
+-- Insert info into Services:
+INSERT INTO Services(name, price, type) VALUES ('serviceA',0.2,'work_day');
+INSERT INTO Services(name, price, type) VALUES ('serviceB',0.24,'work_day');
+INSERT INTO Services(name, price, type) VALUES ('serviceC',0.4,'calendar_day');
+
+
+-- Insert info into Customers
+INSERT INTO Customers(customerID, name, number_free_days, price_service_A, price_service_B,
+                      price_service_C,discount_service_A, discount_service_B, discount_service_C, start_service_A,
+                      start_service_B, start_service_C)
+VALUES ('Customer X','X',null,0.2,null,0.4,0.2,null,0.2,'2019-09-20',null, '2019-09-20');
+
+INSERT INTO Customers(customerID, name, number_free_days, price_service_A, price_service_B,
+                      price_service_C,discount_service_A, discount_service_B, discount_service_C, start_service_A,
+                      start_service_B, start_service_C)
+VALUES ('Customer Y','Y',200,null,0.24,0.4,null,0.3,0.3,null,'2018-01-01','2018-01-01');
+
