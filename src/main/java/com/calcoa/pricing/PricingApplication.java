8e578191641c45c219f87be0be172a711157654c
Inês <ines.calcoa@gmail.com>
2022-04-24 12:21:37 +0100

    openAPI integration: adding annotations and application.properties binding

--- src/main/java/com/calcoa/pricing/PricingApplication.java
+++ src/main/java/com/calcoa/pricing/PricingApplication.java
@@ -1,9 +1,14 @@
 package com.calcoa.pricing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =@Info(title = "Pricing API", version = "1.0", description="API to calculate the billing " +
+		"price of a set of services provided by the company. Input information consists of customerID, startDate and " +
+		"EndDate."))
 public class PricingApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/calcoa/pricing/controller/PricingController.java
+++ src/main/java/com/calcoa/pricing/controller/PricingController.java
@@ -1,28 +1,40 @@
 package com.calcoa.pricing.controller;
 
 import com.calcoa.pricing.operations.PricingOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.HttpURLConnection;
-
 @RestController
+@RequestMapping("/pricing")
+@Tag(name ="Pricing Rest Interface" ,description="Obtain billing values for services")
 public class PricingController {
 
-    @Autowired
-    private PricingOperation pricingOperation;
+    private final PricingOperation pricingOperation;
 
-    @GetMapping("/pricing")
-    public ResponseEntity<String> getPricingPerCustomerId(@RequestParam(name = "customerId") String customerId,
+    public PricingController(PricingOperation pricingOperation) {
+        this.pricingOperation = pricingOperation;
+    }
+
+    @Operation(summary= "Services cost",
+            description = "Billing value by %customer%, between %startDate% and %endDate%")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "operation succeeded"),
+            @ApiResponse(responseCode = "400", description = "input parameters not allowed")}
+    )
+    @PostMapping("/customerId")
+    public ResponseEntity<Double> getPricingPerCustomerId(@RequestParam(name = "customerId") String customerId,
                                                           @RequestParam(name = "startDate") String startDate,
                                                           @RequestParam(name = "endDate") String endDate) {
 
         return ResponseEntity
-                .ok(HttpURLConnection.HTTP_OK
-                        + ": " + pricingOperation.pricingPerCustomer(customerId, startDate, endDate));
+                .ok(pricingOperation.pricingPerCustomer(customerId, startDate, endDate));
     }
 
 }
